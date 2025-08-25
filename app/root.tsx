e5d3765780ac774aa9625b8a4b1968d1923621cf
suhas <suhas@live.ca>
2025-08-24 23:10:40 -0400

    update: sitemap

--- app/root.tsx
+++ app/root.tsx
@@ -44,7 +44,7 @@ export const links: Route.LinksFunction = () => [
     rel: "stylesheet",
     href: "https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap",
   },
-  { rel: "canonical", href: "https://ilovegpacalculator.com" },
+  { rel: "canonical", href: "https://jurassicwords.com" },
 ];
 
 export function Layout({ children }: { children: React.ReactNode }) {
